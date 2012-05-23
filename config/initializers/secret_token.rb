27d5d861ca68d3734d068aab70e31075229b00e6
vikram kumar mishra <vikram.mishra@mindfiresolutions.com>
2012-05-23 10:59:12 +0530

    initial commit for depot application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingCart::Application.config.secret_token = 'e3df052331636b44e57d20ff5cfbb0c38365fa55a1e165a3fd800a30144cd95c05b1a3c7ced3d3cdc5c954155b6ddc6062d1651de1e9f59081833e2c8c6b6f09'
