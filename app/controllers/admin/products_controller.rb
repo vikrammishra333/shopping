1e4b9d46613f7175722c188ddc28a981c5791cb1
vikram kumar mishra <vikram.mishra@mindfiresolutions.com>
2012-06-04 14:19:52 +0530

    will be closed for some time

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -34,79 +34,98 @@ class Admin::ProductsController < AdminController
   def show
     @product = Product.find(params[:id])
 
+    # send data in different format
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @product }
-    end
-  end
+    end # end respond_to
+  end # end action
 
   # GET /products/new
   # GET /products/new.json
+   # Display the form to create a product.
   def new
+    #create a new product object
     @product = Product.new
 
+    # send data in different format
     respond_to do |format|
       format.html{render :layout => false} # new.html.erb
       format.json { render json: @product }
-    end
-  end
+    end # end respond_to
+  end #end action
 
   # GET /products/1/edit
+  # Display the form with pre-existing values to update a product.
   def edit
+     #retrieve category to edit
     @product = Product.find(params[:id])
 
+    # send data in different format
     respond_to do |format|
       format.html{render :layout => false} # new.html.erb 
-    end
-  end
+    end # end respond_to
+  end # end action
 
   # POST /products
   # POST /products.json
+   # Create the product by inserting the values to DB.
   def create
+     #initialize Product object with params
     @product = Product.new(params[:product])
 
+    # send data in different format
     respond_to do |format|
-      if @product.save
+      if @product.save #product saved successfully
 #        format.html { redirect_to admin_product_path(@product), notice: 'Product was successfully created.' }
 #        format.json { render json: @product, status: :created, location: @product }
         format.js
-      else
+      else  # product could not saved successfully
 #        format.html { render action: "new" }
 #        format.json { render json: @product.errors, status: :unprocessable_entity }
         format.js
-      end
-    end
-  end
+      end # end if
+
+    end # end respond_to
+  end # end action
 
   # PUT /products/1
   # PUT /products/1.json
+  # Update the product by updating the values in DB..
   def update
+    #find product by ID.
     @product = Product.find(params[:id])
 
+    # send data in different format
     respond_to do |format|
-      if @product.update_attributes(params[:product])
+      if @product.update_attributes(params[:product]) # product updated successfully
 #        format.html { redirect_to admin_product_path(@product), notice: 'Product was successfully updated.' }
 #        format.json { head :no_content }
          format.js
-      else
+      else  # category could not saved successfully
 #        format.html { render action: "edit" }
 #        format.json { render json: @product.errors, status: :unprocessable_entity }
          format.js
-      end
-    end
-  end
+      end # end if
+    end # end respond_to
+  end # end action
 
   # DELETE /products/1
   # DELETE /products/1.json
+  # Find the product and destroy it
   def destroy
+    #find the product by id
     @product = Product.find(params[:id])
+
+    #destroy the product
     @product.destroy
 
+    #send data in different format
     respond_to do |format|
       format.html { redirect_to admin_products_url }
       format.json { head :no_content }
-    end
-  end
+    end # end respond_to
+  end # end action
 
 
   private
